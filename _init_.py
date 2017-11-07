c0e67ce684d3e4bddabf7956568eeb319945f022
JohnKentDotMe <jonathan.lallouz@gmail.com>
2017-11-07 11:48:17 +0100

    Merge branch 'init-devs' of https://github.com/JohnKentDotMe/prix-carburant into init-devs

--- _init_.py
+++ _init_.py
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
-=======
-#!/usr/bin/env python3 
->>>>>>> eb54a1e70b8110e73f64d6bfaa04fc464aa3c8b2
 # coding:utf-8
 import requests
 import os
 from configparser import ConfigParser
-
-<<<<<<< HEAD
 ####
 # Main file
 
 
-
-=======
->>>>>>> eb54a1e70b8110e73f64d6bfaa04fc464aa3c8b2
 def read_config(section):
     """ Read database configuration file and return a dictionary object
     :param filename: name of the configuration file
@@ -27,11 +18,7 @@ def read_config(section):
     filename = dir + '/config.ini'
     parser = ConfigParser()
     parser.read(filename)
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> eb54a1e70b8110e73f64d6bfaa04fc464aa3c8b2
     # get section, default to mysql
     db = {}
     if parser.has_section(section):
@@ -40,15 +27,9 @@ def read_config(section):
             db[item[0]] = item[1]
     else:
         raise Exception('{0} not found in the {1} file'.format(section, filename))
-<<<<<<< HEAD
 
     return db
 
-=======
- 
-    return db
-    
->>>>>>> eb54a1e70b8110e73f64d6bfaa04fc464aa3c8b2
 def is_alive(site):
     '''function to ckeck if the site is alive'''
     r = requests.get(site , verify=False)
@@ -56,7 +37,3 @@ def is_alive(site):
         print("Welcome to " + site)
     else:
         raise Exception('Error {0}: {1}'.format(r.status_code,r.exception.RequestException))
-<<<<<<< HEAD
-=======
-        
->>>>>>> eb54a1e70b8110e73f64d6bfaa04fc464aa3c8b2
